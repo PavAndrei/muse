e6808d37e22e94d343e105e1ef9d2946c7561f0d
Andrei Pavlov <andrpav9@gmail.com>
2024-06-16 16:42:21 +0400

    validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,31 @@
 
 const form = document.querySelector('form');
 const input = form.querySelector('input');
+const parentDiv = document.querySelector('.academy__inner');
+const statusMessage = document.createElement('div');
+
+
+const message = {
+    success: 'Welcome to the team!',
+    failure: 'Email is invalid',
+}
 
 function validation(form) {
     let result;
+    let inputValue = input.value;
 
-    if (input.value == '') {
-        result = false;
-    } else {
+
+    console.log(inputValue);
+
+    if (inputValue.length >= 3 && inputValue.includes('@') && inputValue.includes('.')) {
         result = true;
+
+        statusMessage.textContent = 'Thank you, we will contact you soon';
+    } else {
+        statusMessage.textContent = 'The email address is too short or does not contain the "@" or "."';
+        result = false;
     }
+    parentDiv.append(statusMessage);
 
     return result;
 }
@@ -18,10 +34,15 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     if (validation(this)) {
-        alert('Welcome to the team!');
+        alert(message.success);
     } else {
-        alert('Email is invalid');
+        alert(message.failure);
     }
+
     input.value = '';
+
+    setTimeout(() => {
+        statusMessage.remove();
+    }, 3000);
 });
 
